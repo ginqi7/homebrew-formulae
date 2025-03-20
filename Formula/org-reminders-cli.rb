b8ce08de999cb679a82ea879a2ff3e97ead1f85e
Qiqi Jin <ginqi7@gmail.com>
2025-03-20 09:46:31 +0800

    feat: Update org-reminders-cli version to v0.0.2

--- Formula/org-reminders-cli.rb
+++ Formula/org-reminders-cli.rb
@@ -1,7 +1,7 @@
 class OrgRemindersCli < Formula
   homepage "https://github.com/ginqi7/org-reminders-cli"
-  url "https://github.com/ginqi7/org-reminders-cli/releases/download/v0.0.1/org-reminders.tar.gz"
-  sha256 "47184f883dd664b9755b75968dd4902f0586f1fd4602c5ec0ba073fa20a0589a"
+  url "https://github.com/ginqi7/org-reminders-cli/releases/download/v0.0.2/org-reminders.tar.gz"
+  sha256 "e5adc62ed0728a704f78b91103bb1001c78e58872f9789dc3cfa1b0b9c0071d6"
 
   def install
     bin.install "org-reminders"
