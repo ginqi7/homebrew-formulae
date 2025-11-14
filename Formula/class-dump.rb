7314bcb6b85c4596d5ada3836f372b47ff8259fe
Qiqi Jin <ginqi7@gmail.com>
2025-11-14 11:46:57 +0800

    add a class-dump Formula.

---
+++ Formula/class-dump.rb
@@ -0,0 +1,9 @@
+class ClassDump < Formula
+  homepage "http://stevenygard.com/projects/class-dump/"
+  url "http://stevenygard.com/download/class-dump-3.5.tar.gz"
+  sha256 "b08134f8efbfaf3bb0c6cb4fc3bd9865b95860d31ea363d9e67a990ed7215bf9"
+
+  def install
+    bin.install "class-dump"
+  end
+end
